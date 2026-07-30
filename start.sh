0cbd4a7a303638227898fc48e61ef3f80faae1aa
Unteress <cjzb20011210@gmail.com>
2026-07-30 13:40:59 -0500

    Fix: Limpieza de caché y conexión DB

--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Ejecutar las migraciones y sembrar la base de datos
+# 1. Limpiar cualquier caché atascada
+echo "Limpiando caché de configuración..."
+php artisan config:clear
+php artisan cache:clear
+
+# 2. Ejecutar las migraciones y sembrar la base de datos
 echo "Ejecutando migraciones..."
 php artisan migrate:fresh --seed --force
 
-# Iniciar el servidor Apache
+# 3. Iniciar el servidor Apache
 echo "Iniciando servidor web..."
 apache2-foreground
